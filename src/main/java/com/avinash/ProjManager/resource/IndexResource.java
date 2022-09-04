55a4b1f2dfc6e1b7dd8b6531ed316b5b3ffdc806
Avibtb <avi.singh.iit01@gmail.com>
2022-09-04 15:24:59 +0530

    Added IndexResource class

---
+++ src/main/java/com/avinash/ProjManager/resource/IndexResource.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.avinash.ProjManager.resource;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author avinash
+ *
+ */
+
+@Controller
+public class IndexResource {
+	
+	@RequestMapping("/")
+	String showIndex() {
+		return "index";
+	}
+
+}
