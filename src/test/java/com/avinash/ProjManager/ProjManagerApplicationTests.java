9c979c69b309c251dc281872a76f6ef3ba35f63a
Avibtb <avi.singh.iit01@gmail.com>
2022-09-04 09:57:25 +0530

    Added spring boilerplate

---
+++ src/test/java/com/avinash/ProjManager/ProjManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.avinash.ProjManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
